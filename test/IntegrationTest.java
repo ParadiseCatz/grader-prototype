2c87b51e7964b057c98f6d5000e9fa9dcb110a56
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-07-27 15:18:55 +0700

    Move to integration test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,8 +1,101 @@
+import controllers.GradingRequest;
+import controllers.Request;
+import models.Grader;
+import models.Submission;
+import models.Verdict;
+import models.ZipSubmission;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
 public class IntegrationTest {
 
     /**
      * add your integration test here
      * in this example we just check if the welcome page is being shown
      */
+
+    @Test
+    public void testJavaAC() {
+        test("test/resources/AC.java", Verdict.AC);
+    }
+
+    @Test
+    public void testJavaRE() {
+        test("test/resources/RE.java", Verdict.RE);
+    }
+
+    @Test
+    public void testJavaWA() {
+        test("test/resources/WA.java", Verdict.WA);
+    }
+
+    @Test
+    public void testJavaTL() {
+        test("test/resources/TL.java", Verdict.TL);
+    }
+
+    @Test
+    public void testCppTL() {
+        test("test/resources/TL.cpp", Verdict.TL);
+    }
+
+    @Test
+    public void testCppCE() {
+        test("test/resources/CE.cpp", Verdict.CE);
+    }
+
+    @Test
+    public void testCppACbf() {
+        test("test/resources/ACbf.cpp", Verdict.TL);
+    }
+
+    @Test
+    public void testZip() {
+        test("test/resources/template.zip", Verdict.WA);
+    }
+
+    @Test
+    public void testInvalidSubmissionType() {
+        test("test/resources/invalid.docx", Verdict.CE);
+    }
+
+    @Test
+    public void testMalicious() {
+        test("test/resources/Connection.java", Verdict.RE);
+        test("test/resources/Deadlock.java", Verdict.TL);
+    }
+
+    public void test(String filename, Verdict expectedResult) {
+        running(fakeApplication(), () -> {
+            File file = new File(filename);
+            File temporaryStorage = new File("/tmp/box/", file.getName());
+            temporaryStorage.getParentFile().mkdirs();
+            try {
+                org.apache.commons.io.FileUtils.copyFile(file, temporaryStorage);
+            } catch (IOException e) {
+                System.out.println("Cannot create temporary directory");
+            }
+
+            Submission submission;
+
+            if (FilenameUtils.getExtension(temporaryStorage.getName()).equals("zip")) {
+                submission = new ZipSubmission(temporaryStorage);
+            } else {
+                submission = new Submission(temporaryStorage);
+            }
+            Request gradingRequest = new GradingRequest(submission);
+            Grader.storeAndExecute(gradingRequest);
+
+            assertEquals(expectedResult, submission.getVerdict());
+
+        });
+    }
 
 }
